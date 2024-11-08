12eb2dad14608795e6c57e02d92e880636488827
Naveen <naveen.jaisankar1999@gmail.com>
2024-11-08 21:58:07 +0000

    Basic email service implemented

---
+++ Backend/NotificationService/src/main/java/com/jobizzz/ctrledu/notification/config/RabbitMQConfig.java
@@ -0,0 +1,40 @@
+package com.jobizzz.ctrledu.notification.config;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.RabbitListenerContainerFactory;
+import org.springframework.amqp.support.converter.DefaultClassMapper;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    public static final String EMAIL_QUEUE = "emailQueue";
+
+    @Bean
+    public Queue emailQueue(){
+        return new Queue(EMAIL_QUEUE,true);
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        Jackson2JsonMessageConverter converter = new Jackson2JsonMessageConverter();
+        converter.setClassMapper(new DefaultClassMapper() {{
+            setTrustedPackages("com.jobizzz.ctrledu.notification.request");
+        }});
+        return converter;
+    }
+
+    @Bean
+    public RabbitListenerContainerFactory<?> rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(jsonMessageConverter());
+        return factory;
+    }
+
+}
