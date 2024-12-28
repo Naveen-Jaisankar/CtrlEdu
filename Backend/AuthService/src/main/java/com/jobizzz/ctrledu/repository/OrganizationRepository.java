85c0f8fe77551ebb11237fef8b532bf325879189
Naveen <naveen.jaisankar1999@gmail.com>
2024-12-28 15:20:55 +0000

    Implemented Thread Local for storing user email id Threadlocal

--- Backend/AuthService/src/main/java/com/jobizzz/ctrledu/repository/OrganizationRepository.java
+++ Backend/AuthService/src/main/java/com/jobizzz/ctrledu/repository/OrganizationRepository.java
@@ -2,6 +2,8 @@ package com.jobizzz.ctrledu.repository;
 
 import com.jobizzz.ctrledu.entity.OrganizationEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OrganizationReporsitory extends JpaRepository<OrganizationEntity,Long> {
 }
